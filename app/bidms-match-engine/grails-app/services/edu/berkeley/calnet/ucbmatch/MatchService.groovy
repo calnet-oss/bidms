2958084506e941c0860c7f6668fd603b1b3498e2
Soeren Berg Glasius <soeren@glasius.dk>
2014-12-04 15:55:54 -0800

    CRN-70 DatabaseService + tests in progress CRN-70 MatchService + tests in progress

---
+++ app/bidms-match-engine/grails-app/services/edu/berkeley/calnet/ucbmatch/MatchService.groovy
@@ -0,0 +1,60 @@
+package edu.berkeley.calnet.ucbmatch
+import edu.berkeley.calnet.ucbmatch.config.MatchConfig
+import edu.berkeley.calnet.ucbmatch.database.Candidate
+import edu.berkeley.calnet.ucbmatch.database.InsertResult
+import edu.berkeley.calnet.ucbmatch.exceptions.RecordExistsException
+import grails.transaction.Transactional
+import groovy.sql.Sql
+import org.joda.time.LocalDateTime
+
+@Transactional
+class MatchService {
+    MatchConfig matchConfig
+
+    @Delegate
+    DatabaseService databaseService
+
+    List<Candidate> findCandidates(String systemOfRecord, String identifier, Map sorAttributes) {
+        List<Candidate> candidates = searchDatabase(systemOfRecord, identifier, sorAttributes, MatchType.CANONICAL)
+        if(!candidates) {
+            candidates = searchDatabase(systemOfRecord,identifier,sorAttributes,MatchType.POTENTIAL)
+        }
+        return candidates
+    }
+
+    InsertResult insertCandidate(String systemOfRecord, String identifier, Map sorAttributes, String referenceId = null, boolean assignNewId = false) {
+        def result = new InsertResult()
+        def idGenerator = getIdGenerator(assignNewId)
+
+        def newRecordReferenceId = referenceId
+        def requestTime = LocalDateTime.now()
+        def resolutionTime = null
+        withTransaction { Sql sql ->
+            def existingRecord = findRecord(systemOfRecord, identifier)
+            if(existingRecord) {
+                if(existingRecord.referenceId) {
+                    log.warn("Existing record found for $systemOfRecord/$identifier during insert.")
+                    throw new RecordExistsException("Existing record found for $systemOfRecord/$identifier during insert.")
+                } else {
+                    log.debug("Existing unreconciled record found for $systemOfRecord/$identifier, replacing.")
+                    removeRecord(systemOfRecord, identifier, sql)
+                }
+            }
+        }
+
+
+
+//        if(matchConfig.matchReference.responseType) {
+//
+//        }
+        return new InsertResult(referenceId: "1234", identifiers: [new Identifier(type: "Enterprise", identifier: "abcd")] )
+    }
+
+    private getIdGenerator(boolean assignNewId) {
+        if(assignNewId && matchConfig.matchReference.idGenerator) {
+            return matchConfig.matchReference.idGenerator
+        } else {
+            return NullIdGenerator
+        }
+    }
+}
