2dac1f8d734e68184dcb699085fef27dd2ef7b69
Soeren Berg Glasius <soeren@glasius.dk>
2015-01-12 17:03:49 -0800

    Implementing: MatchClientService + Tests

--- app/bidms-match-service/grails-app/services/edu/berkeley/match/MatchClientService.groovy
+++ app/bidms-match-service/grails-app/services/edu/berkeley/match/MatchClientService.groovy
@@ -1,10 +1,66 @@
 package edu.berkeley.match
 
 import grails.transaction.Transactional
+import org.springframework.http.HttpStatus
 
 @Transactional
 class MatchClientService {
+    def restClient
+    def grailsApplication
 
-    MatchResponse match(Map<String, String> matchParams) {
+    MatchResponse match(Map<String, String> p) {
+        String matchUrl = grailsApplication.config.match.ucbMatchUrl
+        //[systemOfRecord: 'SIS', sorIdentifier: 'SIS00001', givenName: 'firstName', familyName: 'lastName', dateOfBirth: 'DOB', socialSecurityNumber: 'SSN']
+        def jsonMap = buildJsonMap(p)
+        def response = restClient.post(matchUrl) {
+            accept 'application/json'
+            json(jsonMap)
+        }
+        switch(response.statusCode) {
+            case HttpStatus.NOT_FOUND:
+                return new NoMatch()
+            case HttpStatus.OK:
+                return exactMatch(response.json)
+            case HttpStatus.MULTIPLE_CHOICES:
+                return partialMatch(response.json)
+            default:
+                log.error("Got wrong return code from match engine..")
+                // TODO: Determin what to do in this situation
+                throw new RuntimeException("Got wrong return code from match engine: $response.statusCode.reasonPhrase ($response.statusCode)")
+        }
+    }
+
+    private ExactMatch exactMatch(def json) {
+        new ExactMatch(uid: '') // TODO: Extract UID from json
+    }
+
+    private PartialMatch partialMatch(def json) {
+        new PartialMatch(uids: []) // TODO: Extract UIDS from json
+    }
+
+
+
+    Map buildJsonMap(Map<String, String> p) {
+        def map = [systemOfRecord: p.systemOfRecord, identifier: p.sorIdentifier]
+        if (p.dateOfBirth) {
+            map.dateOfBirth = p.dateOfBirth
+        }
+
+        if (p.familyName || p.givenName) {
+            def name = [type: "official"]
+            if (p.givenName) {
+                name.given = p.givenName
+            }
+            if (p.familyName) {
+                name.family = p.familyName
+            }
+            map.names = [name]
+        }
+        if (p.socialSecurityNumber) {
+            map.identifiers = [
+                    [type: "national", identifier: p.socialSecurityNumber]
+            ]
+        }
+        return map
     }
 }
--- app/bidms-match-service/test/unit/edu/berkeley/match/MatchClientServiceSpec.groovy
+++ app/bidms-match-service/test/unit/edu/berkeley/match/MatchClientServiceSpec.groovy
@@ -1,20 +1,43 @@
 package edu.berkeley.match
-
+import grails.plugins.rest.client.RestBuilder
 import grails.test.mixin.TestFor
+import org.codehaus.groovy.grails.web.servlet.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.test.web.client.MockRestServiceServer
 import spock.lang.Specification
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(MatchClientService)
 class MatchClientServiceSpec extends Specification {
 
+
+    public static final String UCB_MATCH_URL = 'http://localhost/ucb-match/v1/people'
+
     def setup() {
+        grailsApplication.config.match = [ucbMatchUrl: UCB_MATCH_URL]
+        service.restClient = new RestBuilder()
     }
 
-    def cleanup() {
-    }
 
-    void "test something"() {
+    void "test call to match engine where there is no match"() {
+        setup:
+        final mockServer = MockRestServiceServer.createServer(service.restClient.restTemplate)
+        mockServer.expect(requestTo(UCB_MATCH_URL))
+                .andExpect(method(HttpMethod.POST))
+                .andExpect(content().string('{"systemOfRecord":"b","identifier":"BB00002","dateOfBirth":"1930-04-20","names":[{"type":"official","given":"Pat","family":"Stone"}]}'))
+                .andExpect(header(HttpHeaders.ACCEPT, "application/json"))
+                .andRespond(withStatus(HttpStatus.NOT_FOUND))
+
+        when:
+        def result = service.match([systemOfRecord: 'b', sorIdentifier: 'BB00002', dateOfBirth: '1930-04-20', givenName: 'Pat', familyName: 'Stone'])
+
+        then:
+        mockServer.verify()
+        result instanceof NoMatch
     }
 }
