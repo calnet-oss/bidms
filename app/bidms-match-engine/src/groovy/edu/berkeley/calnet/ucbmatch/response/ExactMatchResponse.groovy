e8e9ec451e967bd16f49d6db5e7673f75575f1cd
Soeren Berg Glasius <soeren@glasius.dk>
2015-03-02 13:17:54 -0800

    Test for PersonService where sor/id exists

--- app/bidms-match-engine/src/groovy/edu/berkeley/calnet/ucbmatch/response/ExactMatchResponse.groovy
+++ app/bidms-match-engine/src/groovy/edu/berkeley/calnet/ucbmatch/response/ExactMatchResponse.groovy
@@ -1,10 +1,12 @@
 package edu.berkeley.calnet.ucbmatch.response
 
+import edu.berkeley.calnet.ucbmatch.database.Candidate
+
 import javax.servlet.http.HttpServletResponse
 
 class ExactMatchResponse extends Response {
     int responseCode = HttpServletResponse.SC_OK
-    def responseData
+    Candidate responseData
     Map getJsonMap() {
         [
                 matchingRecord: responseData
--- app/bidms-match-engine/src/groovy/edu/berkeley/calnet/ucbmatch/response/Response.groovy
+++ app/bidms-match-engine/src/groovy/edu/berkeley/calnet/ucbmatch/response/Response.groovy
@@ -4,7 +4,8 @@ import javax.servlet.http.HttpServletResponse
 
 class Response {
     static NOT_FOUND = new Response(responseCode: HttpServletResponse.SC_NOT_FOUND)
-    static CONFLICT = new Response(responseCode: HttpServletResponse.SC_CONFLICT)
     int responseCode
-    def responseData
+    Map getJsonMap() {
+        [:]
+    }
 }
