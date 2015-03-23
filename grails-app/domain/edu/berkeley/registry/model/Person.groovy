6b67cad58a7f2131688ea3482feec96fe242f3b2
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-03-23 14:05:32 -0700

    Add a fix to get .save() and .get() working properly in integration test.
    Grails 2.4.4 seems to require a dummy "String id" be declared, even though
    we're using uid as the primary key.

--- grails-app/domain/edu/berkeley/registry/model/Person.groovy
+++ grails-app/domain/edu/berkeley/registry/model/Person.groovy
@@ -3,6 +3,12 @@ package edu.berkeley.registry.model
 import java.sql.Timestamp
 
 class Person {
+
+    // 'id' can be ignored and is not used, but seems to be necessary to
+    // declare it here as a String in order to avoid a bug in Grails (last
+    // tested with 2.4.4).  uid is instead used as the primary key.
+    String id
+
     String uid
     String dateOfBirthMMDD
     Date dateOfBirth
@@ -10,15 +16,18 @@ class Person {
     Timestamp timeUpdated
 
     static constraints = {
-        uid unique: true
         dateOfBirthMMDD nullable: true
         dateOfBirth nullable: true
+        timeCreated nullable: true // assigned automatically by db trigger
+        timeUpdated nullable: true // assigned automatically by db trigger
     }
 
     static mapping = {
-        id name: 'uid', column: 'uid', generator: 'assigned'
         version false
+        id name: 'uid', column: 'uid', generator: 'assigned'
         dateOfBirthMMDD column: 'dateOfBirthMMDD'
         dateOfBirth column: 'dateOfBirth'
+        timeCreated column: 'timeCreated', insertable: false, updateable: false
+        timeUpdated column: 'timeUpdated', insertable: false, updateable: false
     }
 }
