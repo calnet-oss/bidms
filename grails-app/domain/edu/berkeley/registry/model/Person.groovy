b0e1f0c8726bb632b782c402a10fd20e1cb02833
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-04-09 08:33:31 -0700

    It looks like Grails requires the id property, so what we do is stick uid in the id property, but add a getUid() and setUid() method and make the uid property transient so GORM persists id, but not getUid().

--- grails-app/domain/edu/berkeley/registry/model/Person.groovy
+++ grails-app/domain/edu/berkeley/registry/model/Person.groovy
@@ -2,18 +2,13 @@ package edu.berkeley.registry.model
 
 class Person {
 
-    // 'id' can be ignored and is not used, but seems to be necessary to
-    // declare it here as a String in order to avoid a bug in Grails (last
-    // tested with 2.4.4).  uid is instead used as the primary key.
-    String id
-
-    String uid
+    String id // uid
     String dateOfBirthMMDD
     Date dateOfBirth
     Date timeCreated
     Date timeUpdated
 
-    static hasMany = [ names : PersonName ]
+    static hasMany = [names: PersonName]
 
     static constraints = {
         dateOfBirthMMDD nullable: true
@@ -23,11 +18,19 @@ class Person {
     }
 
     static mapping = {
+        table name: "Person"
         version false
         id name: 'uid', column: 'uid', generator: 'assigned', sqlType: 'VARCHAR(64)'
         dateOfBirthMMDD column: 'dateOfBirthMMDD', sqlType: 'CHAR(4)'
         dateOfBirth column: 'dateOfBirth'
         timeCreated column: 'timeCreated', insertable: false, updateable: false
         timeUpdated column: 'timeUpdated', insertable: false, updateable: false
+        names cascade: "all-delete-orphan"
     }
+
+    static transients = ['uid']
+
+    public String getUid() { return id }
+
+    public void setUid(String uid) { this.id = uid }
 }
