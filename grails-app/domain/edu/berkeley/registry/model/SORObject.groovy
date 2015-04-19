682fcc08797d537d9502b5f0e52c17c74bfab013
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-04-19 10:30:33 -0700

    Merge remote-tracking branch 'origin/master' into bkoehmdev

--- grails-app/domain/edu/berkeley/registry/model/SORObject.groovy
+++ grails-app/domain/edu/berkeley/registry/model/SORObject.groovy
@@ -1,10 +1,16 @@
 package edu.berkeley.registry.model
 
+import groovy.json.JsonSlurper
+
 class SORObject implements Serializable {
 
     String sorPrimaryKey
     Date queryTime
     Person person
+    String objJson
+    Integer jsonVersion
+
+    static transients = ['json']
 
     static SORObject getBySorAndObjectKey(String systemOfRecord, String sorObjectKey) {
         def sorObject = SORObject.where { sor.name == systemOfRecord && sorPrimaryKey == sorObjectKey }.get()
@@ -23,10 +29,15 @@ class SORObject implements Serializable {
         table name: 'SORObject'
         id sqlType: "BIGINT", generator: 'sequence', params: [sequence: 'SORObject_seq']
         version false
+        objJson column: 'objJson', sqlType: 'TEXT'
+        jsonVersion column: 'jsonVersion', sqlType: 'SMALLINT'
         sor column: 'sorId', sqlType: 'SMALLINT'
         sorPrimaryKey column: 'sorObjKey', sqlType: 'VARCHAR(64)'
         queryTime column: 'sorQueryTime'
         person column: 'uid'
+    }
 
+    Map getJson() {
+        new JsonSlurper().parseText(objJson) as Map
     }
 }
