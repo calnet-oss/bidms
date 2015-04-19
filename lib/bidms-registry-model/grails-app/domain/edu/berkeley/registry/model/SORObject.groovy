fbddbf0a999da56ec15afa0fb6ca145e9a7e3ad9
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-04-19 11:01:03 -0700

    person should be a belongsTo

--- lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/SORObject.groovy
+++ lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/SORObject.groovy
@@ -1,17 +1,22 @@
 package edu.berkeley.registry.model
 
+import groovy.json.JsonSlurper
+
 class SORObject implements Serializable {
 
     String sorPrimaryKey
     Date queryTime
-    Person person
+    String objJson
+    Integer jsonVersion
+
+    static transients = ['json']
 
     static SORObject getBySorAndObjectKey(String systemOfRecord, String sorObjectKey) {
         def sorObject = SORObject.where { sor.name == systemOfRecord && sorPrimaryKey == sorObjectKey }.get()
         return sorObject
     }
 
-    static belongsTo = [sor: SOR]
+    static belongsTo = [sor: SOR, person: Person]
 
     static constraints = {
         sorPrimaryKey nullable: false, unique: 'sor'
@@ -23,10 +28,15 @@ class SORObject implements Serializable {
         table name: 'SORObject'
         id sqlType: "BIGINT", generator: 'sequence', params: [sequence: 'SORObject_seq']
         version false
+        objJson column: 'objJson', sqlType: 'TEXT'
+        jsonVersion column: 'jsonVersion', sqlType: 'SMALLINT'
         sor column: 'sorId', sqlType: 'SMALLINT'
         sorPrimaryKey column: 'sorObjKey', sqlType: 'VARCHAR(64)'
         queryTime column: 'sorQueryTime'
         person column: 'uid'
+    }
 
+    Map getJson() {
+        new JsonSlurper().parseText(objJson) as Map
     }
 }
