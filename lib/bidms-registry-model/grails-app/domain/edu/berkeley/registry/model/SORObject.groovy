2e448e3255b938ab4dcf2d807bfe1c45c838cd3d
Zach Legein <zlegein@gmail.com>
2015-04-28 11:23:26 -0500

    CNR-243 - bind jsonb fields with custom hibernate usertype

--- lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/SORObject.groovy
+++ lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/SORObject.groovy
@@ -1,21 +1,23 @@
 package edu.berkeley.registry.model
 
-import net.kaleidos.hibernate.usertype.JsonbMapType
+import edu.berkeley.hibernate.usertype.JSONBType
+import groovy.json.JsonSlurper
 
 class SORObject implements Serializable {
 
     String sorPrimaryKey
     Date queryTime
-    Person person
-    Map objJson
+    String objJson
     Integer jsonVersion
+
+    static transients = ['json']
 
     static SORObject getBySorAndObjectKey(String systemOfRecord, String sorObjectKey) {
         def sorObject = SORObject.where { sor.name == systemOfRecord && sorPrimaryKey == sorObjectKey }.get()
         return sorObject
     }
 
-    static belongsTo = [sor: SOR]
+    static belongsTo = [sor: SOR, person: Person]
 
     static constraints = {
         sorPrimaryKey nullable: false, unique: 'sor'
@@ -25,13 +27,17 @@ class SORObject implements Serializable {
 
     static mapping = {
         table name: 'SORObject'
-        id sqlType: "BIGINT", generator: 'sequence', params: [sequence: 'sorobject_seq']
+        id sqlType: "BIGINT", generator: 'sequence', params: [sequence: 'SORObject_seq']
         version false
-        objJson column: 'objJson', type: JsonbMapType, sqlType: 'jsonb'
+        objJson column: 'objJson', type: JSONBType, sqlType: 'jsonb'
         jsonVersion column: 'jsonVersion', sqlType: 'SMALLINT'
         sor column: 'sorId', sqlType: 'SMALLINT'
         sorPrimaryKey column: 'sorObjKey', sqlType: 'VARCHAR(64)'
         queryTime column: 'sorQueryTime'
         person column: 'uid'
+    }
+
+    Map getJson() {
+        new JsonSlurper().parseText(objJson) as Map
     }
 }
