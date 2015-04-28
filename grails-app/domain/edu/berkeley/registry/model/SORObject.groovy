e9fd4d4266ec3e9b413c4e862d0ae257464af30a
Zach Legein <zlegein@gmail.com>
2015-04-28 11:23:26 -0500

    CNR-243 - bind jsonb fields with custom hibernate usertype

--- grails-app/domain/edu/berkeley/registry/model/SORObject.groovy
+++ grails-app/domain/edu/berkeley/registry/model/SORObject.groovy
@@ -1,14 +1,16 @@
 package edu.berkeley.registry.model
 
-import net.kaleidos.hibernate.usertype.JsonbMapType
+import edu.berkeley.hibernate.usertype.JSONBType
+import groovy.json.JsonSlurper
 
 class SORObject implements Serializable {
 
     String sorPrimaryKey
     Date queryTime
-    Person person
-    Map objJson
+    String objJson
     Integer jsonVersion
+
+    static transients = ['json']
 
     static SORObject getBySorAndObjectKey(String systemOfRecord, String sorObjectKey) {
         def sorObject = SORObject.where { sor.name == systemOfRecord && sorPrimaryKey == sorObjectKey }.get()
@@ -27,11 +29,15 @@ class SORObject implements Serializable {
         table name: 'SORObject'
         id sqlType: "BIGINT", generator: 'sequence', params: [sequence: 'SORObject_seq']
         version false
-        objJson column: 'objJson', type: JsonbMapType, sqlType: 'jsonb'
+        objJson column: 'objJson', type: JSONBType, sqlType: 'jsonb'
         jsonVersion column: 'jsonVersion', sqlType: 'SMALLINT'
         sor column: 'sorId', sqlType: 'SMALLINT'
         sorPrimaryKey column: 'sorObjKey', sqlType: 'VARCHAR(64)'
         queryTime column: 'sorQueryTime'
         person column: 'uid'
+    }
+
+    Map getJson() {
+        new JsonSlurper().parseText(objJson) as Map
     }
 }
---
+++ src/java/edu/berkeley/hibernate/usertype/JSONBType.java
@@ -0,0 +1,79 @@
+package edu.berkeley.hibernate.usertype;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.usertype.UserType;
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+
+/**
+ * A {@link UserType} that persists objects as JSONB.
+ *
+ * static mapping = {
+ *   data type: JSONBType, sqlType: 'jsonb'
+ * }
+ */
+public class JSONBType implements UserType {
+
+    @Override
+    public int[] sqlTypes() {
+        return new int[]{Types.OTHER};
+    }
+
+    @Override
+    public Class<?> returnedClass() {
+        return String.class;
+    }
+
+    @Override
+    public boolean equals(Object x, Object y) throws HibernateException {
+        return ObjectUtils.equals(x, y);
+    }
+
+    @Override
+    public int hashCode(Object x) throws HibernateException {
+        return x == null ? 0 : x.hashCode();
+    }
+
+    @Override
+    public Object nullSafeGet(ResultSet rs, String[] names, SessionImplementor session, Object owner) throws HibernateException, SQLException {
+        return rs.getString(names[0]);
+    }
+
+    @Override
+    public void nullSafeSet(PreparedStatement st, Object value, int index, SessionImplementor session) throws HibernateException, SQLException {
+        st.setObject(index, value, (value == null) ? Types.NULL : Types.OTHER);
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Override
+    public Object deepCopy(Object value) throws HibernateException {
+        if (value == null) return null;
+        return new String((String)value);
+    }
+
+    @Override
+    public boolean isMutable() {
+        return true;
+    }
+
+    @Override
+    public Serializable disassemble(Object value) throws HibernateException {
+        return (Serializable)value;
+    }
+
+    @Override
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        return cached;
+    }
+
+    @Override
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return deepCopy(original);
+    }
+}
