8b7ecff8781d0e84e6166367f6010ddddce950ff
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-05-05 15:16:04 -0700

    change from getString() to getObject() in nullSafeGet() to fix a bug

--- src/java/edu/berkeley/hibernate/usertype/JSONBType.java
+++ src/java/edu/berkeley/hibernate/usertype/JSONBType.java
@@ -42,7 +42,7 @@ public class JSONBType implements UserType {
 
     @Override
     public Object nullSafeGet(ResultSet rs, String[] names, SessionImplementor session, Object owner) throws HibernateException, SQLException {
-        return rs.getString(names[0]);
+        return rs.getObject(names[0]);
     }
 
     @Override
