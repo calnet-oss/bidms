e861fb086f79e30fb4ad7a5cf8ea3ae712df99ea
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-05-07 15:23:39 -0700

    support postgresql jsonb

--- lib/bidms-registry-model/src/java/edu/berkeley/hibernate/usertype/JSONBType.java
+++ lib/bidms-registry-model/src/java/edu/berkeley/hibernate/usertype/JSONBType.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.ObjectUtils;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.usertype.UserType;
+
 import java.io.Serializable;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,12 +14,23 @@ import java.sql.Types;
 
 /**
  * A {@link UserType} that persists objects as JSONB.
- *
+ * <p/>
  * static mapping = {
- *   data type: JSONBType, sqlType: 'jsonb'
+ * data type: JSONBType, sqlType: 'jsonb'
  * }
  */
 public class JSONBType implements UserType {
+
+    // Needed to support PostgreSQL.  Will be null if not using PostgreSQL.
+    static Class<?> pgObjectClass;
+
+    static {
+        try {
+            pgObjectClass = Class.forName("org.postgresql.util.PGobject");
+        } catch (Exception e) {
+            // PostgreSQL class not available
+        }
+    }
 
     @Override
     public int[] sqlTypes() {
@@ -42,7 +54,24 @@ public class JSONBType implements UserType {
 
     @Override
     public Object nullSafeGet(ResultSet rs, String[] names, SessionImplementor session, Object owner) throws HibernateException, SQLException {
-        return (names != null && names.length > 0 ? rs.getObject(names[0]) : null);
+        if (names != null && names.length > 0) {
+            Object result = rs.getObject(names[0]);
+            // Support PostgreSQL: See if PGobject.  (It will be if this is
+            // a JSONB column.)
+            if (result != null && pgObjectClass != null && pgObjectClass.isAssignableFrom(result.getClass())) {
+                // is an instanceof PGobject.  Need to return
+                // PGobject.getValue().
+                try {
+                    result = result.getClass().getMethod("getValue").invoke(result);
+                } catch (Exception e) {
+                    throw new RuntimeException("Couldn't invoke getValue() on instance of " + result.getClass().getName(), e);
+                }
+            } else {
+                // is not an instanceof PGobject.  Return the object as-is.
+                return result;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -50,11 +79,11 @@ public class JSONBType implements UserType {
         st.setObject(index, value, (value == null) ? Types.NULL : Types.OTHER);
     }
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @SuppressWarnings({"rawtypes", "unchecked"})
     @Override
     public Object deepCopy(Object value) throws HibernateException {
         if (value == null) return null;
-        return new String((String)value);
+        return new String((String) value);
     }
 
     @Override
@@ -64,7 +93,7 @@ public class JSONBType implements UserType {
 
     @Override
     public Serializable disassemble(Object value) throws HibernateException {
-        return (Serializable)value;
+        return (Serializable) value;
     }
 
     @Override
