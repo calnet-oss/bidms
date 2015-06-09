e9dd8c6b7bfa73c4fbf912bb1d0489b200b45e9e
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-06-09 14:44:03 -0700

    add timeUpdated

--- grails-app/domain/edu/berkeley/registry/model/PersonSorObjectsSyncKey.groovy
+++ grails-app/domain/edu/berkeley/registry/model/PersonSorObjectsSyncKey.groovy
@@ -5,9 +5,11 @@ class PersonSorObjectsSyncKey {
     String id // uid
     String provisionedJsonHash
     Boolean forceProvision
+    Date timeUpdated // normally updated by a trigger, but sometimes we want to force persistence by modifying this column
 
     static constraints = {
         provisionedJsonHash nullable: true
+        timeUpdated nullable: true // is NOT NULL but will be filled in by the trigger if null
     }
 
     static mapping = {
@@ -16,5 +18,6 @@ class PersonSorObjectsSyncKey {
         id column: 'uid', generator: 'assigned', sqlType: 'VARCHAR(64)'
         provisionedJsonHash column: 'provisionedJsonHash', sqlType: 'TEXT'
         forceProvision column: 'forceProvision', sqlType: 'BOOLEAN'
+        timeUpdated column: 'timeUpdated'
     }
 }
