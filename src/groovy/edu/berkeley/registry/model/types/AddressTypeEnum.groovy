bf9ae7e240812f710fdf028bff8568f1e2f78419
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-06-25 15:52:13 -0700

    have type enums implement TypeEnum interface and add tests for them

--- src/groovy/edu/berkeley/registry/model/types/AddressTypeEnum.groovy
+++ src/groovy/edu/berkeley/registry/model/types/AddressTypeEnum.groovy
@@ -1,14 +1,34 @@
 package edu.berkeley.registry.model.types
 
+import edu.berkeley.registry.model.AddressType
+
 /**
  * The different types of addresses from SOR JSON that translate into a
  * AddressType.
  */
-enum AddressTypeEnum {
+enum AddressTypeEnum implements TypeEnum<AddressType> {
     directoryPrimaryAddress,
     directorySecondaryAddress
 
+    AddressType addressType
+
+    AddressType get() {
+        if (!addressType)
+            addressType = AddressType.findByAddressTypeName(name())
+        if (addressType == null)
+            throw new RuntimeException("AddressType ${name()} could not be found")
+        return addressType
+    }
+
     String getName() {
         return name()
+    }
+
+    Integer getId() {
+        return get().id
+    }
+
+    static AddressTypeEnum getEnum(AddressType t) {
+        return valueOf(AddressTypeEnum, t?.addressTypeName)
     }
 }
--- src/groovy/edu/berkeley/registry/model/types/TelephoneTypeEnum.groovy
+++ src/groovy/edu/berkeley/registry/model/types/TelephoneTypeEnum.groovy
@@ -1,14 +1,34 @@
 package edu.berkeley.registry.model.types
 
+import edu.berkeley.registry.model.TelephoneType
+
 /**
  * The different types of telephones from SOR JSON that translate into a
  * TelephoneType.
  */
-enum TelephoneTypeEnum {
+enum TelephoneTypeEnum implements TypeEnum<TelephoneType> {
     directoryOfficePhone,
     directoryFaxPhone
 
+    TelephoneType telephoneType
+
+    TelephoneType get() {
+        if (!telephoneType)
+            telephoneType = TelephoneType.findByTelephoneTypeName(name())
+        if (telephoneType == null)
+            throw new RuntimeException("TelephoneType ${name()} could not be found")
+        return telephoneType
+    }
+
     String getName() {
         return name()
+    }
+
+    Integer getId() {
+        return get().id
+    }
+
+    static TelephoneTypeEnum getEnum(TelephoneType t) {
+        return valueOf(TelephoneTypeEnum, t?.telephoneTypeName)
     }
 }
