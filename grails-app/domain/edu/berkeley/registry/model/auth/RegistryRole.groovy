db3ac40b8a4c616411ba1cfe4d8699d569b84e73
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-08-03 07:02:01 -0700

    RegistryUser auth

---
+++ grails-app/domain/edu/berkeley/registry/model/auth/RegistryRole.groovy
@@ -0,0 +1,40 @@
+package edu.berkeley.registry.model.auth
+
+class RegistryRole implements Serializable {
+    private static final long serialVersionUID = 1
+
+    Integer id
+    String authority
+
+    RegistryRole(String authority) {
+        this()
+        this.authority = authority
+    }
+
+    @Override
+    int hashCode() {
+        authority?.hashCode() ?: 0
+    }
+
+    @Override
+    boolean equals(other) {
+        is(other) || (other instanceof RegistryRole && other.authority == authority)
+    }
+
+    @Override
+    String toString() {
+        authority
+    }
+
+    static constraints = {
+        authority blank: false, unique: true, size: 1..127
+    }
+
+    static mapping = {
+        table name: "RegistryRole"
+        version false
+        id column: 'id', generator: 'sequence', params: [sequence: 'registryRole_seq'], sqlType: 'INTEGER'
+        authority column: "authority", sqlType: 'VARCHAR(127)'
+
+    }
+}
