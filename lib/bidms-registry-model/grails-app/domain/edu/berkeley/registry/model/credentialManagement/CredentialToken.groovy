fddafaaf892e305db971f6d0c0c7545792f2219a
Søren Berg Glasius <soeren@glasius.dk>
2015-10-01 16:51:05 +0200

    CNR-460 Added IdentifierType to be able to know what identifierType the token is related to

--- lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/credentialManagement/CredentialToken.groovy
+++ lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/credentialManagement/CredentialToken.groovy
@@ -1,17 +1,22 @@
 package edu.berkeley.registry.model.credentialManagement
 
+import edu.berkeley.registry.model.IdentifierType
 import edu.berkeley.registry.model.Person
 import edu.berkeley.registry.model.RandomStringUtil
+import edu.berkeley.util.domain.DomainUtil
+import org.hibernate.FetchMode
 
 import static edu.berkeley.registry.model.RandomStringUtil.CharTemplate.*
 
 class CredentialToken {
     String token
+    IdentifierType identifierType
     Person person
     Date expiryDate
     static constraints = {
         token nullable: false, maxSize: 32
-        person nullable: false, unique: true
+        identifierType nullable: false
+        person nullable: false, unique: 'identifierType'
         expiryDate nullable: false
     }
 
@@ -21,7 +26,8 @@ class CredentialToken {
 
         id column: 'id', generator: 'sequence', params: [sequence: 'CredentialToken_seq'], sqlType: 'BIGINT'
         token column: 'token'
-        person column: 'personUid'
+        identifierType column: 'identifierTypeId', sqlType: 'SMALLINT', fetch: FetchMode.JOIN
+        person column: CredentialToken.getUidColumnName(), sqlType: 'VARCHAR(64)'
         expiryDate column: 'expiryDate'
     }
 
@@ -30,5 +36,13 @@ class CredentialToken {
             token = RandomStringUtil.randomString(10, UPPER_ALPHA, LOWER_ALPHA, NUMERIC)
         }
     }
+
+    // Makes the column name unique in test mode to avoid GRAILS-11600
+    // 'unique' bug.  See https://jira.grails.org/browse/GRAILS-11600 and
+    // comments in DomainUtil.
+    static String getUidColumnName() {
+        return DomainUtil.testSafeColumnName("CredentialToken", "uid")
+    }
+
 
 }
--- lib/bidms-registry-model/test/unit/edu/berkeley/registry/model/credentialManagement/CredentialTokenSpec.groovy
+++ lib/bidms-registry-model/test/unit/edu/berkeley/registry/model/credentialManagement/CredentialTokenSpec.groovy
@@ -1,17 +1,19 @@
 package edu.berkeley.registry.model.credentialManagement
 
+import edu.berkeley.registry.model.IdentifierType
 import edu.berkeley.registry.model.Person
 import grails.test.mixin.Mock
 import spock.lang.Specification
 
-@Mock([CredentialToken, Person])
+@Mock([CredentialToken, Person, IdentifierType])
 class CredentialTokenSpec extends Specification {
     def "test that object validates because beforeValidate assigns token"() {
         given:
+        def identifierType = new IdentifierType(idName: 'sisStudentId').save(failOnError: true, flush: true)
         def person = new Person(uid: '123').save(failOnError: true, flush: true)
 
         when:
-        def credentialToken = new CredentialToken(person: person, expiryDate: new Date() +1)
+        def credentialToken = new CredentialToken(person: person, identifierType: identifierType, expiryDate: new Date() + 1)
 
         then:
         credentialToken.validate()
