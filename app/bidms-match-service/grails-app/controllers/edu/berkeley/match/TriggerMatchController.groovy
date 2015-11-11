0c46edcf48af7289102dc5004559a254891b3362
Søren Berg Glasius <soeren@glasius.dk>
2015-11-11 16:16:48 +0100

    CNR-519 A little more logging

--- app/bidms-match-service/grails-app/controllers/edu/berkeley/match/TriggerMatchController.groovy
+++ app/bidms-match-service/grails-app/controllers/edu/berkeley/match/TriggerMatchController.groovy
@@ -6,8 +6,10 @@ class TriggerMatchController {
     def newSORConsumerService
     def matchPerson(SorKeyDataCommand sorKeyDataCommand) {
         if(sorKeyDataCommand.hasErrors())  {
+            log.debug("could not trigger a match: $sorKeyDataCommand.errors")
             render(status: HttpServletResponse.SC_BAD_REQUEST)
         } else {
+            log.debug("Sor Key Data attributes. $sorKeyDataCommand.attributes")
             newSORConsumerService.matchPerson(sorKeyDataCommand.sorObject, sorKeyDataCommand.attributes)
             render(status: HttpServletResponse.SC_OK)
         }
