e6c06ff638d2349dcccca6eb73e29e7d74ff4e16
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-11-25 12:02:49 -0800

    back out temporary debugging logging

--- app/bidms-match-service/grails-app/services/edu/berkeley/match/MatchClientService.groovy
+++ app/bidms-match-service/grails-app/services/edu/berkeley/match/MatchClientService.groovy
@@ -25,10 +25,6 @@ class MatchClientService {
     PersonMatch match(Map<String, String> p) {
         String matchUrl = grailsApplication.config.rest.matchEngine.url
         def jsonMap = buildJsonMap(p)
-        // FIXME: JSON Exceptions being thrown here
-        restClient.restTemplate.messageConverters.each { HttpMessageConverter converter ->
-            log.info("messageConverter: ${converter.getClass().name}, supportedMediaTypes=${converter.supportedMediaTypes}")
-        }
         def response = restClient.post(matchUrl) {
             accept 'application/json'
             contentType "application/json"
