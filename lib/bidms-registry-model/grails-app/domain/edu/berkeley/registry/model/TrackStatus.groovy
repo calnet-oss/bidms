c8ceb476fe6a6b676fb8245fb851db9ec8e5efbe
Søren Berg Glasius <soeren@glasius.dk>
2016-01-28 11:29:17 +0100

    CM-171 Added TrackStatus to registry-model, bumped to 0.51-SNAPSHOT

---
+++ lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/TrackStatus.groovy
@@ -0,0 +1,46 @@
+package edu.berkeley.registry.model
+
+import edu.berkeley.util.domain.DomainUtil
+import edu.berkeley.util.domain.transform.ConverterConfig
+import edu.berkeley.util.domain.transform.LogicalEqualsAndHashCode
+
+@ConverterConfig(excludes = ["person"])
+@LogicalEqualsAndHashCode(excludes = ["person"])
+class TrackStatus implements Comparable {
+    Long id
+    String trackStatusType
+    Date timeCreated
+    String description
+
+    static belongsTo = [person: Person]
+
+    static constraints = {
+        person unique: ['trackStatusType']
+        trackStatusType nullable: false, size: 1..64
+        timeCreated nullable: true // assigned automatically by db trigger
+        description nullable: true, size: 1..256
+    }
+
+    static mapping = {
+        table name: "TrackStatus"
+        version false
+        id column: 'id', generator: 'sequence', params: [sequence: 'TrackStatus_seq'], sqlType: 'BIGINT'
+        person column: TrackStatus.getUidColumnName(), sqlType: 'VARCHAR(64)'
+        timeCreated column: 'timeCreated', insertable: false, updateable: false
+        trackStatusType column: 'trackStatusType', sqlType: 'VARCHAR(64)'
+        description column: 'description', sqlType: 'VARCHAR(256)'
+
+
+    }
+    // Makes the column name unique in test mode to avoid GRAILS-11600
+    // 'unique' bug.  See https://jira.grails.org/browse/GRAILS-11600 and
+    // comments in DomainUtil.
+    static String getUidColumnName() {
+        return DomainUtil.testSafeColumnName("TrackStatus", "uid")
+    }
+
+    int compareTo(obj) {
+        return logicalHashCode() <=> obj.logicalHashCode() ?: hashCode() <=> obj.hashCode()
+    }
+
+}
