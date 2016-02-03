7b355f2550a44b1d9c3afd9d30df7046be8e63f6
Søren Berg Glasius <soeren@glasius.dk>
2016-02-03 23:18:45 +0100

    CM-149 DelegateProxy getProxyForIdentifier

--- lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/DelegateProxy.groovy
+++ lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/DelegateProxy.groovy
@@ -1,5 +1,6 @@
 package edu.berkeley.registry.model
 
+import edu.berkeley.registry.model.types.DelegateProxyTypeEnum
 import edu.berkeley.util.domain.transform.ConverterConfig
 import edu.berkeley.util.domain.transform.LogicalEqualsAndHashCode
 import org.hibernate.FetchMode
@@ -13,13 +14,35 @@ class DelegateProxy implements Comparable {
     DelegateProxyType delegateProxyType
 
     // information about the proxy delegate
-    static belongsTo = [delegateProxyPerson: Person] // the Person object (and thus uid) of the proxy delegate
+    static belongsTo = [person: Person] // the Person object (and thus uid) of the proxy delegate
     String sourceProxyId // primary key of the delegate proxy in the source system (ex: the CS_DELEGATE SCC_DA_PRXY_ID)
     SORObject delegateProxySorObject // The optional SORObject the delegate data is sourced from
     String delegateProxySecurityKey // The security key generated for the proxy to claim an account (ex: the CS_DELEGATE SCC_DA_SECURTY_KEY)
 
     // information about the person the delegate is a proxy for
     String proxyForId // the source system identifier this delegate is a proxy for (ex: the CS_DELEGATE EMPLID)
+
+    /**
+     * Lazy loads the proxyForIdentifier
+     * @return
+     */
+    Identifier getProxyForIdentifier() {
+        String identifierTypeName = DelegateProxyTypeEnum.getEnum(delegateProxyType).identifierTypeEnum.name
+        IdentifierType identifierType = IdentifierType.findByIdName(identifierTypeName)
+        Identifier identifier = Identifier.findByIdentifierTypeAndIdentifier(identifierType, proxyForId)
+        return identifier
+    }
+
+    /**
+     * Lazy loads the proxyForPerson
+     * @return
+     */
+    Person getProxyForPerson() {
+        def identifier = getProxyForIdentifier()
+        identifier?.person
+    }
+
+    static transients = ['proxyForIdentifier', 'proxyForPerson']
 
     static constraints = {
         delegateProxySorObject nullable: true
@@ -30,7 +53,7 @@ class DelegateProxy implements Comparable {
         table name: "DelegateProxy"
         version false
         id column: 'id', generator: 'sequence', params: [sequence: 'DelegateProxy_seq'], sqlType: 'BIGINT'
-        delegateProxyPerson column: 'delegateProxyUid', sqlType: 'VARCHAR(64)'
+        person column: 'delegateProxyUid', sqlType: 'VARCHAR(64)'
         delegateProxyType column: 'delegateProxyTypeId', sqlType: 'SMALLINT', fetch: FetchMode.JOIN
         delegateProxySorObject column: 'delegateProxySorObjectId', sqlType: 'BIGINT'
         sourceProxyId column: 'sourceProxyId', sqlType: 'VARCHAR(64)'
