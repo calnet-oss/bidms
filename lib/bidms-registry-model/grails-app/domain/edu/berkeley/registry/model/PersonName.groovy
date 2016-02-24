d316c30aa5204b5b567cbbb8bb2b1cf84846a7b3
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2016-02-23 16:35:12 -0800

    add prefix and multi-value jsonb honorifics

--- lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/PersonName.groovy
+++ lib/bidms-registry-model/grails-app/domain/edu/berkeley/registry/model/PersonName.groovy
@@ -1,7 +1,11 @@
 package edu.berkeley.registry.model
+
+import edu.berkeley.hibernate.usertype.JSONBType
 import edu.berkeley.util.domain.DomainUtil
 import edu.berkeley.util.domain.transform.ConverterConfig
 import edu.berkeley.util.domain.transform.LogicalEqualsAndHashCode
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
 import org.hibernate.FetchMode
 
 @ConverterConfig(excludes = ["person", "sorObject"])
@@ -11,23 +15,27 @@ class PersonName implements Comparable {
     Long id
     NameType nameType
     SORObject sorObject
-    String honorific
+    String prefix
     String givenName
     String middleName
     String surName
     String suffix
     String fullName
+    String honorifics // This is a JSON rray of strings.  Stored in PostGreSQL as JSONB.
     boolean isPrimary
+
+    static transients = ['honorificsAsList']
 
     static belongsTo = [person: Person]
 
     static constraints = {
         person unique: ['sorObject', 'nameType']
-        honorific nullable: true
+        prefix nullable: true
         givenName nullable: true
         middleName nullable: true
         surName nullable: true
         suffix nullable: true
+        honorifics nullable: true
         fullName nullable: true
     }
 
@@ -38,11 +46,12 @@ class PersonName implements Comparable {
         person column: PersonName.getUidColumnName(), sqlType: 'VARCHAR(64)'
         nameType column: 'nameTypeId', sqlType: 'SMALLINT', fetch: FetchMode.JOIN
         sorObject column: 'sorObjectId', sqlType: 'BIGINT'
-        honorific column: 'honorific', sqlType: 'VARCHAR(32)'
+        prefix column: 'prefix', sqlType: 'VARCHAR(32)'
         givenName column: 'givenName', sqlType: 'VARCHAR(127)'
         middleName column: 'middleName', sqlType: 'VARCHAR(127)'
         surName column: 'surName', sqlType: 'VARCHAR(127)'
         suffix column: 'suffix', sqlType: 'VARCHAR(32)'
+        honorifics column: 'honorifics', type: JSONBType, sqlType: 'jsonb'
         fullName column: 'fullName', sqlType: 'VARCHAR(255)'
         isPrimary column: 'isPrimary', sqlType: 'BOOLEAN'
     }
@@ -56,5 +65,13 @@ class PersonName implements Comparable {
 
     int compareTo(obj) {
         return logicalHashCode() <=> obj.logicalHashCode() ?: hashCode() <=> obj.hashCode()
+    }
+
+    List getHonorificsAsList() {
+        return (honorifics ? new JsonSlurper().parseText(honorifics) as List : null)
+    }
+
+    void setHonorificsAsList(List honorificsAsList) {
+        this.honorifics = (honorificsAsList ? new JsonBuilder(honorificsAsList).toString() : null)
     }
 }
