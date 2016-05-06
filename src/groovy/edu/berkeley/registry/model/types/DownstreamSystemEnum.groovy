bfc5976a083d53d084c1f4a496e68e2475c1952b
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2016-05-06 11:20:06 -0700

    CNR-840: Added DownstreamSystem and DownstreamObject

---
+++ src/groovy/edu/berkeley/registry/model/types/DownstreamSystemEnum.groovy
@@ -0,0 +1,26 @@
+package edu.berkeley.registry.model.types
+
+import edu.berkeley.registry.model.DownstreamSystem
+
+enum DownstreamSystemEnum implements TypeEnum<DownstreamSystem> {
+    LDAP
+
+    DownstreamSystem get() {
+        return DownstreamSystem.findByName(name())
+    }
+
+    String getName() {
+        return name()
+    }
+
+    Integer getId() {
+        return get().id
+    }
+
+    /**
+     * Get the DownstreamSystemEnum by passing in a DownstreamSystem.
+     */
+    static DownstreamSystemEnum getEnum(DownstreamSystem sys) {
+        return valueOf(DownstreamSystemEnum, sys?.name)
+    }
+}
---
+++ test/unit/edu/berkeley/registry/model/DownstreamObjectSpec.groovy
@@ -0,0 +1,70 @@
+package edu.berkeley.registry.model
+
+import edu.berkeley.registry.model.types.DownstreamSystemEnum
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import groovy.json.JsonBuilder
+import spock.lang.Specification
+
+@TestFor(DownstreamObject)
+@Mock([DownstreamObject, DownstreamSystem, Person])
+class DownstreamObjectSpec extends Specification {
+    DownstreamSystem testDownstreamSystem
+    Person testPerson
+
+    def setup() {
+        testDownstreamSystem = new DownstreamSystem(name: DownstreamSystemEnum.LDAP.name())
+        testDownstreamSystem.save(flush: true, failOnError: true)
+
+        testPerson = new Person(uid: "person1")
+        testPerson.save(flush: true, failOnError: true)
+    }
+
+    def "test that a DownstreamObject can be found when exists"() {
+        given:
+            assert DownstreamSystem.get(testDownstreamSystem.id) != null
+            def obj = new DownstreamObject(
+                    person: Person.get("person1"),
+                    system: DownstreamSystem.findById(testDownstreamSystem.id),
+                    systemPrimaryKey: '123',
+                    objJson: '{}'
+            ).save(flush: true, failOnError: true)
+
+        expect:
+            obj.id
+
+        and:
+            DownstreamObject.findBySystemAndSystemPrimaryKey(DownstreamSystem.findByName(DownstreamSystemEnum.LDAP.name()), '123')
+    }
+
+    def "test that finding a DownstreamObject with unknown system or key returns null"() {
+        given:
+            def obj = new DownstreamObject(
+                    person: Person.get("person1"),
+                    system: DownstreamSystem.findByName(DownstreamSystemEnum.LDAP.name()),
+                    systemPrimaryKey: '123',
+                    objJson: '{}'
+            ).save(flush: true, failOnError: true)
+
+        expect:
+            obj.id
+
+        and:
+            !DownstreamObject.findBySystemAndSystemPrimaryKey(null, '123')
+    }
+
+    def "test parsing json"() {
+        given:
+            def json = new JsonBuilder([id: 3, name: 'archer']).toString()
+            def obj = new DownstreamObject(
+                    person: Person.get("person1"),
+                    system: DownstreamSystem.findByName(DownstreamSystemEnum.LDAP.name()),
+                    systemPrimaryKey: '123',
+                    objJson: json
+            ).save(flush: true, failOnError: true)
+
+        expect:
+            obj.json.id == 3
+            obj.json.name == 'archer'
+    }
+}
