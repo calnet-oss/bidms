5831748c66c663b8a9e8f4836ee02f07ed1d3b6c
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2017-02-27 14:14:13 -0800

    CNR-1363 WIP: Modify test to reproduce collection persistence bug for the jobAppointments collection.

--- lib/bidms-registry-model/registry-model-test/src/test/groovy/edu/berkeley/registry/model/PersonSpec.groovy
+++ lib/bidms-registry-model/registry-model-test/src/test/groovy/edu/berkeley/registry/model/PersonSpec.groovy
@@ -3,23 +3,41 @@ package edu.berkeley.registry.model
 import grails.test.mixin.Mock
 import spock.lang.Specification
 
-@Mock([Person])
+@Mock([AppointmentType, SOR, SORObject, Person, JobAppointment])
 class PersonSpec extends Specification {
     private static final Map opts = [failOnError: true, flush: true]
 
-    /**
-     * Since using 'uid' as the identifier property, this confirms Person
-     * has a getId() and setId() method that retrieves and sets the uid
-     * property.
-     */
     void "save test"() {
         given:
+        AppointmentType apptType = new AppointmentType(apptTypeName: "testApptType")
+        SOR sor = new SOR(name: "HRMS")
+        SORObject sorObject = new SORObject(sorPrimaryKey: "123", jsonVersion: 1, queryTime: "2015-07-15T20:15:33Z", sor: sor, objJson: "{}")
         Person person = new Person(uid: "uid123")
+        person.addToJobAppointments(new JobAppointment(
+                apptIdentifier: "appt123",
+                jobCode: "TEST",
+                hireDate: Date.parse("yyyy-MM-dd", "2017-02-27"),
+                sorObject: sorObject,
+                apptType: apptType,
+                person: person
+        ))
 
         when:
+        apptType.save(opts)
+        sor.save(opts)
+        sorObject.save(opts)
         person.save(opts)
+        person.refresh()
 
         then:
+        /**
+         * Since using 'uid' as the identifier property, this confirms Person
+         * has a getId() and setId() method that retrieves and sets the uid
+         * property.
+         */
         person.id
+        /* Confirm jobAppointments collection persisted */
+        person.jobAppointments?.size() > 0
+        person.jobAppointments.first().apptIdentifier == "appt123"
     }
 }
