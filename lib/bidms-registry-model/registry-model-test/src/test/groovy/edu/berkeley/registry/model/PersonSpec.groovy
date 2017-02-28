62c3e8ece7fc07ca81882a57279b254b0ba0b5e1
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2017-02-27 17:49:20 -0800

    CNR-1365: Renamed to PersonHibernateSpec to make room for a PersonSpec that uses @Mock instead of HibernateSpec.

--- lib/bidms-registry-model/registry-model-test/src/test/groovy/edu/berkeley/registry/model/PersonSpec.groovy
+++ lib/bidms-registry-model/registry-model-test/src/test/groovy/edu/berkeley/registry/model/PersonSpec.groovy
@@ -3,15 +3,20 @@ package edu.berkeley.registry.model
 import grails.test.mixin.Mock
 import spock.lang.Specification
 
+/**
+ * This is similar to PersonHibernateSpec, but we are testing non-hibernate
+ * testing here (i.e., we extend Specification, not HibernateSpec and use
+ * @Mock ) .
+ */
 @Mock([AppointmentType, SOR, SORObject, Person, JobAppointment])
 class PersonSpec extends Specification {
     private static final Map opts = [failOnError: true, flush: true]
 
-    void "save test"() {
+    void "save test using Hibernate in unit test environment"() {
         given:
         AppointmentType apptType = new AppointmentType(apptTypeName: "testApptType")
         SOR sor = new SOR(name: "HRMS")
-        SORObject sorObject = new SORObject(sorPrimaryKey: "123", jsonVersion: 1, queryTime: "2015-07-15T20:15:33Z", sor: sor, objJson: "{}")
+        SORObject sorObject = new SORObject(sorPrimaryKey: "123", jsonVersion: 1, queryTime: Date.parse("yyyy-MM-dd", "2015-07-15"), sor: sor, objJson: "{}")
         Person person = new Person(uid: "uid123")
         person.addToJobAppointments(new JobAppointment(
                 apptIdentifier: "appt123",
