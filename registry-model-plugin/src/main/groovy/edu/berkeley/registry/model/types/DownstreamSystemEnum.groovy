0dd5a17c4da2c770843420485ef6cb03f9eb74f5
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2017-05-24 11:16:43 -0700

    CNR-1466: Add LDAP_NAMESPACE DownstreamSystem

--- registry-model-plugin/src/main/groovy/edu/berkeley/registry/model/types/DownstreamSystemEnum.groovy
+++ registry-model-plugin/src/main/groovy/edu/berkeley/registry/model/types/DownstreamSystemEnum.groovy
@@ -3,7 +3,7 @@ package edu.berkeley.registry.model.types
 import edu.berkeley.registry.model.DownstreamSystem
 
 enum DownstreamSystemEnum implements TypeEnum<DownstreamSystem> {
-    LDAP
+    LDAP, LDAP_NAMESPACE
 
     DownstreamSystem get() {
         return DownstreamSystem.findByName(name())
