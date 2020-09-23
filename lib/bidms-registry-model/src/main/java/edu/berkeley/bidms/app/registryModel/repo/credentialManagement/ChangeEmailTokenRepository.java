d8d79b9d0f9cc3383975063c2735998ed13f2ba6
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2020-09-23 14:26:47 -0700

    registry-model: add findByTokenAndPerson to ChangeEmailTokenRepository

--- lib/bidms-registry-model/src/main/java/edu/berkeley/bidms/app/registryModel/repo/credentialManagement/ChangeEmailTokenRepository.java
+++ lib/bidms-registry-model/src/main/java/edu/berkeley/bidms/app/registryModel/repo/credentialManagement/ChangeEmailTokenRepository.java
@@ -26,6 +26,7 @@
  */
 package edu.berkeley.bidms.app.registryModel.repo.credentialManagement;
 
+import edu.berkeley.bidms.app.registryModel.model.Person;
 import edu.berkeley.bidms.app.registryModel.model.credentialManagement.ChangeEmailToken;
 import edu.berkeley.bidms.registryModel.repo.ExtendedRepository;
 
@@ -33,4 +34,5 @@ import edu.berkeley.bidms.registryModel.repo.ExtendedRepository;
  * Repository for {@link ChangeEmailToken} entities.
  */
 public interface ChangeEmailTokenRepository extends ExtendedRepository<ChangeEmailToken, Long> {
+    ChangeEmailToken findByTokenAndPerson(String token, Person person);
 }
