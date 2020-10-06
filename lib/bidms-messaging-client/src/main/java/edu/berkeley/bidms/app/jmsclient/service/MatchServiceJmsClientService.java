09cf0bd54c3a3c8a5c079c9eba3f722bc52fcce2
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2020-10-06 11:26:15 -0700

    messaging-client: add MatchServiceJmsClientService

---
+++ lib/bidms-messaging-client/src/main/java/edu/berkeley/bidms/app/jmsclient/service/MatchServiceJmsClientService.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020, Regents of the University of California and
+ * contributors.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.berkeley.bidms.app.jmsclient.service;
+
+import edu.berkeley.bidms.app.common.config.properties.BidmsConfigProperties;
+import edu.berkeley.bidms.app.common.config.properties.jms.endpoint.JmsEndpointConfigProperties;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class MatchServiceJmsClientService {
+    private BidmsConfigProperties bidmsConfigProperties;
+
+    public MatchServiceJmsClientService(BidmsConfigProperties bidmsConfigProperties) {
+        this.bidmsConfigProperties = bidmsConfigProperties;
+    }
+
+    public void newSorObject(final JmsTemplate jmsTemplate, final Map sorKeyDataMap) {
+        JmsEndpointConfigProperties jmsEndpointConfigProperties = bidmsConfigProperties.getJms().getMatchService().getNewSorObject();
+        jmsTemplate.convertAndSend(jmsEndpointConfigProperties.getQueueName(), sorKeyDataMap);
+    }
+}
