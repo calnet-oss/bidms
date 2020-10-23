1c14a4e82978f70fb3afcfe52081bfe7df3a8e18
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2020-10-23 11:10:32 -0700

    bidms-boot: use SecurityServiceConfigurer implementations to configure security for each app

--- bidms-boot/src/main/java/edu/berkeley/bidms/app/config/BidmsApplicationSecurityConfig.java
+++ bidms-boot/src/main/java/edu/berkeley/bidms/app/config/BidmsApplicationSecurityConfig.java
@@ -26,7 +26,15 @@
  */
 package edu.berkeley.bidms.app.config;
 
+import edu.berkeley.bidms.app.springsecurity.config.DownstreamProvisioningSecurityConfigurer;
+import edu.berkeley.bidms.app.springsecurity.config.MatchEngineSecurityConfigurer;
+import edu.berkeley.bidms.app.springsecurity.config.MatchServiceSecurityConfigurer;
+import edu.berkeley.bidms.app.springsecurity.config.RegistryProvisioningSecurityConfigurer;
+import edu.berkeley.bidms.app.springsecurity.config.RegistryServiceSecurityConfigurer;
+import edu.berkeley.bidms.app.springsecurity.config.SgsSecurityConfigurer;
 import edu.berkeley.bidms.app.springsecurity.service.RegistryUserDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,23 +44,83 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class BidmsApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final Logger log = LoggerFactory.getLogger(BidmsApplicationSecurityConfig.class);
+
     @Autowired
     private RegistryUserDetailsService registryUserDetailsService;
 
+    // === Optional beans that customize the security configuration for each service (app) ===
+    // The app, if it wishes to override, can instantiate these beans in the app's @Configuration class.
+    @Autowired(required = false)
+    private SgsSecurityConfigurer sgsSecurityConfigurer;
+    @Autowired(required = false)
+    private MatchEngineSecurityConfigurer matchEngineSecurityConfigurer;
+    @Autowired(required = false)
+    private MatchServiceSecurityConfigurer matchServiceSecurityConfigurer;
+    @Autowired(required = false)
+    private RegistryProvisioningSecurityConfigurer registryProvisioningSecurityConfigurer;
+    @Autowired(required = false)
+    private DownstreamProvisioningSecurityConfigurer downstreamProvisioningSecurityConfigurer;
+    @Autowired(required = false)
+    private RegistryServiceSecurityConfigurer registryServiceSecurityConfigurer;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        var rm = http
                 .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/hello/**").permitAll()
-                .antMatchers("/sgs/**").hasAuthority("sorGateway")
-                .antMatchers("/match-engine/**").hasAuthority("ucbMatch")
-                .antMatchers("/match-service/**").hasAuthority("registryMatchService")
-                .antMatchers("/registry-provisioning/**").hasAuthority("registryProvisioning")
-                .antMatchers("/bidms-downstream/changePassword/**").hasAuthority("bidmsDownstreamChangePassword")
-                .antMatchers("/bidms-downstream/**").hasAuthority("bidmsDownstream")
-                /*.antMatchers("/**").denyAll()*/
-                .anyRequest().denyAll()
+                .authorizeRequests();
+
+        rm.antMatchers("/hello/**").permitAll();
+
+        if (sgsSecurityConfigurer != null) {
+            log.info("SGS security rules being applied from " + sgsSecurityConfigurer.getClass().getName());
+            sgsSecurityConfigurer.applyRules(rm);
+        } else {
+            log.info("SGS default security rules being applied");
+            SgsSecurityConfigurer.defaultRules(rm);
+        }
+
+        if (matchEngineSecurityConfigurer != null) {
+            log.info("Match engine security rules being applied from " + matchEngineSecurityConfigurer.getClass().getName());
+            matchEngineSecurityConfigurer.applyRules(rm);
+        } else {
+            log.info("Match engine default security rules being applied");
+            MatchEngineSecurityConfigurer.defaultRules(rm);
+        }
+
+        if (matchServiceSecurityConfigurer != null) {
+            log.info("Match service security rules being applied from " + matchServiceSecurityConfigurer.getClass().getName());
+            matchServiceSecurityConfigurer.applyRules(rm);
+        } else {
+            log.info("Match service default security rules being applied");
+            MatchServiceSecurityConfigurer.defaultRules(rm);
+        }
+
+        if (registryProvisioningSecurityConfigurer != null) {
+            log.info("Registry provisioning security rules being applied from " + registryProvisioningSecurityConfigurer.getClass().getName());
+            registryProvisioningSecurityConfigurer.applyRules(rm);
+        } else {
+            log.info("Registry provisioning default security rules being applied");
+            RegistryProvisioningSecurityConfigurer.defaultRules(rm);
+        }
+
+        if (downstreamProvisioningSecurityConfigurer != null) {
+            log.info("Downstream provisioning security rules being applied from " + downstreamProvisioningSecurityConfigurer.getClass().getName());
+            downstreamProvisioningSecurityConfigurer.applyRules(rm);
+        } else {
+            log.info("Downstream provisioning default security rules being applied");
+            DownstreamProvisioningSecurityConfigurer.defaultRules(rm);
+        }
+
+        if (registryServiceSecurityConfigurer != null) {
+            log.info("Registry service security rules being applied from " + registryServiceSecurityConfigurer.getClass().getName());
+            registryServiceSecurityConfigurer.applyRules(rm);
+        } else {
+            log.info("Registry service default security rules being applied");
+            RegistryServiceSecurityConfigurer.defaultRules(rm);
+        }
+
+        rm.anyRequest().denyAll()
                 .and().httpBasic();
     }
 
