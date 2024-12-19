be9db4e776ecb57c76594fe1320dca14a3bf814d
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2024-12-19 14:59:40 -0800

    downstream: move some methods out of ProvisionLdapService and into BaseDownstreamProvisionService

--- app/bidms-downstream/src/main/groovy/edu/berkeley/bidms/app/downstream/service/ldap/ProvisionLdapService.groovy
+++ app/bidms-downstream/src/main/groovy/edu/berkeley/bidms/app/downstream/service/ldap/ProvisionLdapService.groovy
@@ -29,6 +29,7 @@ package edu.berkeley.bidms.app.downstream.service.ldap
 import edu.berkeley.bidms.app.common.config.properties.provisionContext.ProvisioningContextConfigProperties
 import edu.berkeley.bidms.app.common.config.properties.provisionContext.ProvisioningContextProperties
 import edu.berkeley.bidms.app.downstream.config.properties.DownstreamConfigProperties
+import edu.berkeley.bidms.app.downstream.service.BaseDownstreamProvisionService
 import edu.berkeley.bidms.app.downstream.service.DownstreamProvisionService
 import edu.berkeley.bidms.app.jmsclient.service.DownstreamProvisioningJmsClientService
 import edu.berkeley.bidms.app.registryModel.model.DownstreamObject
@@ -45,12 +46,10 @@ import edu.berkeley.bidms.connector.ldap.UidObjectDefinition
 import edu.berkeley.bidms.downstream.jms.DownstreamProvisionJmsTemplate
 import edu.berkeley.bidms.downstream.ldap.LdapConflictResolutionAware
 import edu.berkeley.bidms.downstream.ldap.SystemUidObjectDefinition
-import edu.berkeley.bidms.downstream.service.DownstreamProvisioningService
 import edu.berkeley.bidms.downstream.service.DownstreamSystemNotFoundException
-import edu.berkeley.bidms.downstream.service.ProvisioningResult
-import groovy.json.JsonSlurper
 import groovy.sql.Sql
 import groovy.util.logging.Slf4j
+import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
@@ -58,19 +57,15 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.ldap.core.LdapTemplate
 import org.springframework.ldap.core.support.LdapContextSource
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Propagation
-import org.springframework.transaction.annotation.Transactional
 
-import jakarta.annotation.PostConstruct
 import javax.naming.Name
 import javax.sql.DataSource
-import java.sql.Timestamp
 
 @Slf4j
 // If you wish to override this bean, create your own with @Service("provisionLdapService")
 @ConditionalOnMissingBean(name = "provisionLdapService")
 @Service("edu.berkeley.bidms.app.downstream.service.ldap.ProvisionLdapService")
-class ProvisionLdapService<PC extends ProvisioningContextProperties> implements DownstreamProvisioningService<PC> {
+class ProvisionLdapService<PC extends ProvisioningContextProperties> extends BaseDownstreamProvisionService<PC> {
 
     @Value('${bidms.downstream.app-name}')
     String APP_NAME
@@ -83,18 +78,6 @@ class ProvisionLdapService<PC extends ProvisioningContextProperties> implements 
 
     @Autowired
     DownstreamProvisionService provisionService
-
-    @Autowired
-    DataSource dataSource
-
-    @Autowired
-    DownstreamProvisioningJmsClientService downstreamProvisioningJmsClientService
-
-    @Autowired
-    DownstreamProvisionJmsTemplate downstreamJmsTemplate
-
-    @Autowired
-    DownstreamSystemRepository downstreamSystemRepository
 
     @Autowired
     DownstreamObjectRepository downstreamObjectRepository
@@ -121,6 +104,15 @@ class ProvisionLdapService<PC extends ProvisioningContextProperties> implements 
     private static final Map<String, Long> batchCountMap = [changed: 0, unchanged: 0]
     private static final Map<String, Long> totalBatchTimeSecondsMap = [changed: 0, unchanged: 0]
     private static final int batchSize = 1000
+
+    ProvisionLdapService(
+            DataSource dataSource,
+            DownstreamSystemRepository downstreamSystemRepository,
+            DownstreamProvisionJmsTemplate downstreamJmsTemplate,
+            DownstreamProvisioningJmsClientService downstreamProvisioningJmsClientService
+    ) {
+        super(dataSource, downstreamSystemRepository, downstreamJmsTemplate, downstreamProvisioningJmsClientService)
+    }
 
     @Override
     PC getProvisioningContext() {
@@ -149,123 +141,6 @@ class ProvisionLdapService<PC extends ProvisioningContextProperties> implements 
         return [DownstreamSystemEnum.LDAP.name]
     }
 
-    // we want one-tx-per-uid, not one tx-per-batch
-    @Transactional(propagation = Propagation.NEVER)
-    @Override
-    ProvisioningResult provisionBulk(String eventId, String downstreamSystemName, boolean isSynchronous) {
-        int sendQueueCount = 0
-        int unchangedCount = 0
-        Sql sql = new Sql(dataSource)
-        try {
-            DownstreamSystem downstreamSystem = null
-            downstreamSystem = downstreamSystemRepository.findByName(downstreamSystemName?.toUpperCase())
-            if (!downstreamSystem) {
-                throw new DownstreamSystemNotFoundException(downstreamSystemName?.toUpperCase())
-            }
-
-            // to provision
-            int expectedToProvisionCount = sql.firstRow(bulkToProvisionCountSql, [downstreamSystem.id]).count
-            log.info("Expecting to provision $expectedToProvisionCount entries to $downstreamSystemName ${isSynchronous ? 'synchronously' : 'asynchronously'} for eventId $eventId")
-            sql.eachRow(bulkToProvisionSql, [downstreamSystem.id]) { row ->
-                if (asyncPersistUid(eventId, downstreamSystem, row.uid, row.globUniqId, (isSynchronous ? (Map<String, Object>) new JsonSlurper().parseText(row.objJson as String) : null), (isSynchronous ? row.hash : null), false, isSynchronous)) {
-                    sendQueueCount++
-                } else {
-                    unchangedCount++
-                }
-            }
-
-            // to delete
-            int expectedToDeleteCount = sql.firstRow(bulkToDeleteCountSql, [downstreamSystem.id]).count
-            if (expectedToDeleteCount) {
-                log.info("Expecting to delete $expectedToDeleteCount entries from $downstreamSystemName ${isSynchronous ? 'synchronously' : 'asynchronously'} for eventId $eventId")
-                sql.eachRow(bulkToDeleteSql, [downstreamSystem.id]) { row ->
-                    // even though DeletedDownstreamObject has the full
-                    // objJson, for deletes, the only thing that is needed
-                    // in the map is uid
-                    if (asyncPersistUid(eventId, downstreamSystem, row.uid, null, [uid: row.uid], null, true, isSynchronous)) {
-                        sendQueueCount++
-                    } else {
-                        unchangedCount++
-                    }
-                }
-            }
-        }
-        finally {
-            sql.close()
-        }
-
-        if (isSynchronous) {
-            log.info("Provisioned $sendQueueCount entries to $downstreamSystemName synchronously for eventId $eventId")
-        } else {
-            log.info("Sent $sendQueueCount entries to the provisioning queue for $downstreamSystemName asynchronously for eventId $eventId")
-        }
-
-        return new ProvisioningResult(downstreamSystemName: downstreamSystemName, count: sendQueueCount, unchangedCount: unchangedCount, synchronous: isSynchronous)
-    }
-
-    @Transactional(rollbackFor = Exception)
-    @Override
-    ProvisioningResult provisionUid(String eventId, String downstreamSystemName, String uid, boolean forceAsynchronous, boolean skipIfUnchanged) {
-        Sql sql = new Sql(dataSource)
-        try {
-            DownstreamSystem downstreamSystem = downstreamSystemRepository.findByName(downstreamSystemName.toUpperCase())
-            if (!downstreamSystem) {
-                throw new DownstreamSystemNotFoundException(downstreamSystemName?.toUpperCase())
-            }
-            boolean isSynchronous = !forceAsynchronous
-            def row = sql.firstRow(uidToProvisionSql, [downstreamSystem.id, uid])
-            if (row) {
-                if (!skipIfUnchanged || row.hash != row.provisionedHash || row.forceProvision) {
-                    boolean wasModifiedOrSent = asyncPersistUid(eventId, downstreamSystem, uid, row.globUniqId, (isSynchronous ? (Map<String, Object>) new JsonSlurper().parseText(row.objJson as String) : null), (isSynchronous ? row.hash : null), false, isSynchronous)
-                    if (isSynchronous) {
-                        // if synchronous then it was an explicit request,
-                        // i.e., from a REST call
-                        log.trace("Provisioned uid $uid to $downstreamSystemName synchronously for eventId $eventId")
-                    } else {
-                        // if asynchronous then we're processing off a queue
-                        // in bulk
-                        log.trace("Sent uid $uid to $downstreamSystemName local queue asynchronously for eventId $eventId")
-                    }
-                    return new ProvisioningResult(downstreamSystemName: downstreamSystemName, count: (wasModifiedOrSent ? 1 : 0), unchangedCount: (!wasModifiedOrSent ? 1 : 0), synchronous: isSynchronous)
-                } else {
-                    // skipIfUnchanged is true and hash == provisionedHash,
-                    // so no change is indicated and we should skip
-                    return new ProvisioningResult(downstreamSystemName: downstreamSystemName, count: 0, unchangedCount: 1, synchronous: isSynchronous)
-                }
-            } else {
-                // The uid may be a pending delete or an already-deleted or
-                // unowned (ownershipLevel <= 0)
-                row = sql.firstRow(uidToDeleteSql, [downstreamSystem.id, uid])
-                if (row && !row.timeDeletedDownstream) {
-                    // not deleted downstream yet
-                    boolean wasModifiedOrSent = asyncPersistUid(eventId, downstreamSystem, uid, null, [uid: uid], null, true, isSynchronous)
-                    if (isSynchronous) {
-                        // if synchronous then it was an explicit request,
-                        // i.e., from a REST call
-                        log.info("Deleted uid $uid from $downstreamSystemName synchronously for eventId $eventId")
-                    } else {
-                        // if asynchronous then we're processing off a queue
-                        // in bulk
-                        log.debug("Sent an expected delete for uid $uid to $downstreamSystemName local queue asynchronously for eventId $eventId")
-                    }
-                    return new ProvisioningResult(downstreamSystemName: downstreamSystemName, count: (wasModifiedOrSent ? 1 : 0), unchangedCount: (!wasModifiedOrSent ? 1 : 0), synchronous: isSynchronous)
-                } else if (row) {
-                    // already deleted
-                    log.info("Request was received to reprovision already-deleted uid $uid to $downstreamSystemName.  Ignoring.")
-                    return new ProvisioningResult(downstreamSystemName: downstreamSystemName, count: 0, unchangedCount: 1, synchronous: isSynchronous)
-                } else {
-                    // uid is not found OR ownershipLevel is <= 0 in either
-                    // DownstreamObject or DeletedDownstreamObject
-                    log.debug("Request was received to reprovision uid $uid to $downstreamSystemName but either the uid doesn't exist or it is unowned, meaning it has an ownershipLevel of <= 0 in either DownstreamObject or DeletedDownstreamObject.")
-                    return new ProvisioningResult(downstreamSystemName: downstreamSystemName, count: 0, unchangedCount: 1, synchronous: isSynchronous)
-                }
-            }
-        }
-        finally {
-            sql.close()
-        }
-    }
-
     protected ProvisionLdapServiceCallbackContext createProvisionLdapServiceCallbackContext(int downstreamSystemId) {
         return new ProvisionLdapServiceCallbackContext(
                 systemTypeName: ((SystemUidObjectDefinition) uidObjectDefinition).systemType,
@@ -273,25 +148,12 @@ class ProvisionLdapService<PC extends ProvisioningContextProperties> implements 
         )
     }
 
-    protected LdapRequestContext createLdapRequestContext(String eventId, ProvisionLdapServiceCallbackContext context) {
-        return new LdapRequestContext(new LdapTemplate(connector.contextSource), eventId, uidObjectDefinition, context)
-    }
-
-    protected LdapRequestContext createLdapRequestContext(String eventId, int downstreamSystemId) {
-        return createLdapRequestContext(eventId, createProvisionLdapServiceCallbackContext(downstreamSystemId))
-    }
-
-    protected LdapRequestContext createLdapRequestContext(String eventId, ProvisionLdapServiceCallbackContext context, LdapContextSource ldapContextSource) {
-        return new LdapRequestContext(new LdapTemplate(ldapContextSource), eventId, uidObjectDefinition, context)
-    }
-
     /**
      * @return true if a LDAP modification actually occurred
      */
-
-    // This is a method that is called from non-transactional methods within
-    // this class so we have to manage the start of the transaction manually.
+    @Override
     boolean persistUid(String eventId, int downstreamSystemId, String uid, String globUniqId, Map<String, Object> jsonObject, Long hash, boolean isDelete) {
+        // This is a method that is called from non-transactional methods so we have to manage the start of the transaction manually.
         boolean wasModified = false
         Sql sql = new Sql(dataSource)
         try {
@@ -307,7 +169,7 @@ class ProvisionLdapService<PC extends ProvisioningContextProperties> implements 
                     }
                     if (uidObjectDefinition instanceof LdapConflictResolutionAware) {
                         // You may wish to make sure that there aren't other
-                        // entries that will conflict with this object. 
+                        // entries that will conflict with this object.
                         // I.e., the schema may be enforcing attribute
                         // uniqueness on certain attributes and you need to
                         // resolve any collisions before trying to persist
@@ -335,73 +197,16 @@ class ProvisionLdapService<PC extends ProvisioningContextProperties> implements 
         return wasModified
     }
 
-    @SuppressWarnings("GrMethodMayBeStatic")
-    String getBulkToProvisionSql() {
-        return "SELECT uid, hash, objJson, globUniqId FROM DownstreamObjectToProvisionView WHERE systemId = ?"
+    protected LdapRequestContext createLdapRequestContext(String eventId, ProvisionLdapServiceCallbackContext context) {
+        return new LdapRequestContext(new LdapTemplate(connector.contextSource), eventId, uidObjectDefinition, context)
     }
 
-    String getBulkToProvisionCountSql() {
-        return "SELECT count(*) AS count FROM (${bulkToProvisionSql}) sub"
+    protected LdapRequestContext createLdapRequestContext(String eventId, int downstreamSystemId) {
+        return createLdapRequestContext(eventId, createProvisionLdapServiceCallbackContext(downstreamSystemId))
     }
 
-    String getUidToProvisionSql() {
-        return "SELECT uid AS uid, hash, provisionedHash, objJson, forceProvision, globUniqId FROM DownstreamObject WHERE systemId = ? AND uid = ? AND ownershipLevel > 0"
-    }
-
-    String getUidToDeleteSql() {
-        return "SELECT uid, timeDeletedDownstream FROM DeletedDownstreamObject WHERE systemId = ? AND ownershipLevel > 0 AND uid = ?"
-    }
-
-    String getBulkToDeleteSql() {
-        return "SELECT uid FROM DeletedDownstreamObject WHERE systemId = ? AND timeDeletedDownstream IS NULL AND ownershipLevel > 0"
-    }
-
-    String getBulkToDeleteCountSql() {
-        return "SELECT count(*) AS count FROM (${bulkToDeleteSql}) sub"
-    }
-
-    @Transactional(rollbackFor = Exception)
-    @SuppressWarnings("GrMethodMayBeStatic")
-    void updateDownstreamObject(Sql sql, int downstreamSystemId, String uid, long hash) {
-        if (sql.executeUpdate("UPDATE DownstreamObject SET provisionedHash = ?, forceProvision = ? WHERE systemId = ? AND sysObjKey = ?" as String, [hash, false, downstreamSystemId, uid]) != 1) {
-            log.warn("Couldn't find DownstreamObject for uid $uid, downstreamSystemId=$downstreamSystemId while provisioning.  Was it just deleted?")
-        }
-    }
-
-    @Transactional(rollbackFor = Exception)
-    void markDeletedDownstreamObjectAsDeletedDownstream(Sql sql, int downstreamSystemId, String uid) {
-        if (sql.executeUpdate("UPDATE DeletedDownstreamObject SET timeDeletedDownstream = ? WHERE systemId = ? AND sysObjKey = ?" as String, [new Timestamp(new Date().time), downstreamSystemId, uid]) != 1) {
-            log.warn("Couldn't find DeletedDownstreamObject for uid $uid, downstreamSystemId=$downstreamSystemId while processing deletes.  Was it just deleted?")
-        }
-    }
-
-    /**
-     * @return If running synchronously and true, that means LDAP was
-     *         modified.  If running asynchronously and true, then a message
-     *         was sent to the local provisioning queue.  Should always
-     *         return true if running asynchronously.
-     */
-    boolean asyncPersistUid(String eventId, DownstreamSystem downstreamSystem, String uid, String globUniqId, Map<String, Object> jsonObject, Long hash, boolean isDelete, boolean forceSynchronous) {
-        if (!(downstreamSystem.name in accepts())) {
-            throw new RuntimeException("this service doesn't accept provisioning objects from ${downstreamSystem.name}")
-        }
-        boolean isSynchronous = forceSynchronous
-        if (isSynchronous) {
-            if (!jsonObject) {
-                throw new RuntimeException("jsonObject can't be null when persisting synchronously")
-            }
-            if (!isDelete && hash == null) {
-                throw new RuntimeException("hash can't be null when persisting a non-delete synchronously")
-            }
-            return persistUid(eventId, downstreamSystem.id, uid, globUniqId, jsonObject, hash, isDelete)
-        } else {
-            Map<String, Object> headers = [
-                    eventId   : eventId,
-                    fromSource: "local"
-            ]
-            downstreamProvisioningJmsClientService.provisionUid(downstreamJmsTemplate, downstreamSystem.name, uid, headers)
-            return true
-        }
+    protected LdapRequestContext createLdapRequestContext(String eventId, ProvisionLdapServiceCallbackContext context, LdapContextSource ldapContextSource) {
+        return new LdapRequestContext(new LdapTemplate(ldapContextSource), eventId, uidObjectDefinition, context)
     }
 
     String getSearchBase() {
